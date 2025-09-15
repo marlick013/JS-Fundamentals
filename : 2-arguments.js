2dd01fc912e93703c4719f14a0e17236264a7f1f
marlick013 <malikhammedowolabidimeji@gmail.com>
2025-09-15 00:34:21 -0700

    Update : 2-arguments.js

--- : 2-arguments.js
+++ : 2-arguments.js
@@ -1,9 +1,11 @@
-const args = process.argv.slice(2);
-
-if (args.length === 0) {
-  console.log("No argument");
-} else if (args.length === 1) {
-  console.log("Argument found");
-} else {
-  console.log("Arguments found");
+function factorial(n) {
+    if (isNaN(n) || n <0) {
+        return 1;
+    }
+if (n === 0 || n === 1){return 1;
 }
+return n * factorial(n - 1);
+} 
+
+const arg = parseInt(process.argv[2]);
+console.log(factorial(arg));
