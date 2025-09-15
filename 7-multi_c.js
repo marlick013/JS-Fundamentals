95340fabeb953665d6b8878ba900403b7d89b923
marlick013 <malikhammedowolabidimeji@gmail.com>
2025-09-15 00:58:02 -0700

    Update 7-multi_c.js

--- 7-multi_c.js
+++ 7-multi_c.js
@@ -3,7 +3,7 @@ const { argv } = require('node:process');
 const x = parseInt(argv[2]);
 let output = "";
 
-if (isNaN(x)) {
+if (isNaN(x) || x <= 0) {
   console.log("Missing number of occurrences");
 } else {
   for (let i = 0; i < x; i++) {
