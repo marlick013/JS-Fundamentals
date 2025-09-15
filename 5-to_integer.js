df2a98bd0fde3ae8ea68504141d28c2cc0943cbb
marlick013 <malikhammedowolabidimeji@gmail.com>
2025-09-15 01:38:15 -0700

    Update 5-to_integer.js

--- 5-to_integer.js
+++ 5-to_integer.js
@@ -1,8 +1,8 @@
-const arg = process.argv[2];         // This gets the argument from the command line
-const number = parseInt(arg);        // This tries to turn the argument into an integer
+const arg = process.argv[2];         
+const number = parseInt(arg);     
 
 if (isNaN(number)) {
-  console.log('Not a number');       // If it’s not a number, say so
+  console.log('Not a number');     
 } else {
-  console.log(My number: ${number}); // If it is a number, show it
+  console.log(My number: ${number});
 }
