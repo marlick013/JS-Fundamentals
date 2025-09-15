e0b2d63086004b66d6685a3eb6f17e804adcae75
marlick013 <malikhammedowolabidimeji@gmail.com>
2025-09-15 00:30:15 -0700

    Update 3-value_argument.js

--- 3-value_argument.js
+++ 3-value_argument.js
@@ -2,4 +2,10 @@ const { argv } = require('node:process');
 
 if (argv[2] === undefined) {
   console.log("No argument");
+} else if (argv[3] === undefined) {
+  console.log("Argument found");
+  console.log(argv[2]);
+} else {
+  console.log("Arguments found");
+  console.log(argv.slice(2).join(' '));
 }
