dd47b5dc2228a8fb2e5f5c1b8bd986df348a00ce
marlick013 <malikhammedowolabidimeji@gmail.com>
2025-09-15 01:01:34 -0700

    Update 7-multi_c.js

--- 7-multi_c.js
+++ 7-multi_c.js
@@ -3,11 +3,10 @@ const { argv } = require('node:process');
 const x = parseInt(argv[2]);
 let output = "";
 
-if (isNaN(x) || x <= 0) {
+if (isNaN(x)) {
   console.log("Missing number of occurrences");
 } else {
-  for (let i = 0; i < x; i++) {
+  for (let i = 0; i < Math.abs(x); i++) {
     output += "C is fun\n";
   }
-  console.log(output.trim());
-}
+  console.log(output.trim())
